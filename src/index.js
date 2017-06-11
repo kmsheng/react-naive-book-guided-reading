47c624cfb0431e3f9707113cd772790fae7f239a
kmsheng <kmsh3ng@gmail.com>
2017-06-11 15:06:59 +0800

    remove content of index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('.like-btn');
-const buttonText = button.querySelector('.like-text');
-
-let isLiked = false;
-
-button.addEventListener('click', () => {
-  isLiked = ! isLiked;
-  buttonText.innerHTML = isLiked ? '取消' : '点赞';
-}, false);
