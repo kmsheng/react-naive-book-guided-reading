c4c1bc7ef35e2428dd7db530ce2adb26b458f375
kmsheng <kmsh3ng@gmail.com>
2017-06-12 14:44:59 +0800

    add example for createElement

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React, {Component} from 'react';
+import React, {Component, createElement} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1 className="title">React 小書</h1>
-      </div>
+      createElement(
+        "div",
+        null,
+        createElement(
+          "h1",
+          {className: 'title'},
+          "React 小書"
+        )
+      )
     );
   }
 }
