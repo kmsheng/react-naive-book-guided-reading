03a9b6b783ac51d0b8eb9df731b5252d6b7f7c40
kmsheng <kmsh3ng@gmail.com>
2017-06-12 15:25:40 +0800

    add demo code for handling events

--- src/index.js
+++ src/index.js
@@ -2,55 +2,28 @@ import React, {Component, createElement} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Title extends Component {
-  render () {
-    return (
-      <h1>React 小书</h1>
-    );
-  }
-}
-
-class Header extends Component {
-  render () {
-    return (
-      <div>
-        <Title />
-        <h2>This is Header</h2>
-      </div>
-    );
-  }
-}
-
-class Main extends Component {
-  render () {
-    return (
-      <div>
-        <h2>This is main content</h2>
-      </div>
-    );
-  }
-}
-
-class Footer extends Component {
-  render () {
-    return (
-      <div>
-        <h2>This is footer</h2>
-      </div>
-    );
-  }
-}
-
 class Index extends Component {
-  render () {
+
+  constructor(props) {
+    // https://facebook.github.io/react/docs/react-component.html#constructor
+    // react/src/isomorphic/modern/class/ReactBaseClasses.js
+    super(props);
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleButtonClick = () => this.setState({message: 'you clicked me !'});
+
+  render() {
+    const {message} = this.state;
     return (
       <div>
-        <Header />
-        <Main />
-        <Footer />
+        <span>{message}</span>
+        <button onClick={this.handleButtonClick}>click me</button>
       </div>
-    );
-  }
+    )
+  };
 }
 
 ReactDOM.render(<Index />, document.getElementById('root'));
