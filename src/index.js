5d2130b199b53604ecccf8c702850f2af13eb093
kmsheng <kmsh3ng@gmail.com>
2017-06-12 15:12:47 +0800

    re-style

--- src/index.js
+++ src/index.js
@@ -6,38 +6,38 @@ class Title extends Component {
   render () {
     return (
       <h1>React 小书</h1>
-    )
+    );
   }
 }
 
 class Header extends Component {
   render () {
     return (
-    <div>
-      <Title />
-      <h2>This is Header</h2>
-    </div>
-    )
+      <div>
+        <Title />
+        <h2>This is Header</h2>
+      </div>
+    );
   }
 }
 
 class Main extends Component {
   render () {
     return (
-    <div>
-      <h2>This is main content</h2>
-    </div>
-    )
+      <div>
+        <h2>This is main content</h2>
+      </div>
+    );
   }
 }
 
 class Footer extends Component {
   render () {
     return (
-    <div>
-      <h2>This is footer</h2>
-    </div>
-    )
+      <div>
+        <h2>This is footer</h2>
+      </div>
+    );
   }
 }
 
@@ -49,7 +49,7 @@ class Index extends Component {
         <Main />
         <Footer />
       </div>
-    )
+    );
   }
 }
 
