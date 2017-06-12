73432d5374d61dc3a1a87ad8711f01c6e1ebb291
kmsheng <kmsh3ng@gmail.com>
2017-06-12 14:23:18 +0800

    add nested example

--- src/index.js
+++ src/index.js
@@ -2,26 +2,25 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const BOOKS = [
-  {id: 1, name: 'Understanding ECMAScript 6: The Definitive Guide for JavaScript Developers'},
-  {id: 2, name: 'You Don\'t Know JS: Up & Going'},
-  {id: 3, name: 'You Don\'t Know JS: Scope & Closures (Paperback)'},
-  {id: 4, name: 'You Don\'t Know JS: this & Object Prototypes (Paperback)'},
-  {id: 5, name: 'You Don\'t Know JS: Types & Grammar (Paperback)'},
-  {id: 6, name: 'You Don\'t Know JS: Async & Performance (Paperback) '},
-  {id: 7, name: 'You Don\'t Know JS: ES6 & Beyond (Paperback) '}
+const BOOK_LIS = [
+  <li key="book-row-1">Understanding ECMAScript 6: The Definitive Guide for JavaScript Developers</li>,
+  [
+    <li key="book-row-2">I am inside</li>,
+    <li key="book-row-3">I am inside2</li>,
+    [
+      <li key="book-row-4">I am inside3</li>
+    ]
+  ]
 ];
 
 class App extends Component {
 
   render() {
 
-    const lis = BOOKS.map((book) => (<li key={`book-row-${book.id}`}>{book.name}</li>));
-
     return (
       <div>
         <h1 className="title">React 小書</h1>
-        <ul>{lis}</ul>
+        <ul>{BOOK_LIS}</ul>
       </div>
     );
   }
