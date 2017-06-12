44d9d752aaa14e7d857c107b8008f4a51c7e6daf
kmsheng <kmsh3ng@gmail.com>
2017-06-12 15:52:21 +0800

    add demo for props usage

--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
 import React, {Component, createElement} from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
+
+class LikeButton extends Component {
+
+  static propTypes = {
+    thumbUpCount: PropTypes.number.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLiked: false
+    };
+  }
+
+  handleButtonClick = () => this.setState((prevState) => ({isLiked: ! prevState.isLiked}));
+
+  render() {
+    const {thumbUpCount} = this.props;
+    const {isLiked} = this.state;
+    return (
+      <button class="like-btn" onClick={this.handleButtonClick}>
+        <span class="like-text">{isLiked ? '取消' : '點讚'}</span>
+        <span>{'👍'.repeat(thumbUpCount)}</span>
+      </button>
+    );
+  }
+}
 
 class Index extends Component {
 
   constructor(props) {
-    // https://facebook.github.io/react/docs/react-component.html#constructor
-    // react/src/isomorphic/modern/class/ReactBaseClasses.js
     super(props);
-    this.state = {
-      message: ''
-    };
+    this.state = {thumbUpCount: 1};
   }
 
-  handleButtonClick = () => this.setState({message: 'you clicked me !'});
+  handleIncreaseThumbUpButtonClick = () => this.setState((prevState) => ({thumbUpCount: prevState.thumbUpCount + 1}));
 
   render() {
-    const {message} = this.state;
+    const {thumbUpCount} = this.state;
     return (
       <div>
-        <span>{message}</span>
-        <button onClick={this.handleButtonClick}>click me</button>
+        <button onClick={this.handleIncreaseThumbUpButtonClick}>Incease Thumb Up Count</button>
+        <LikeButton thumbUpCount={thumbUpCount} />
       </div>
     )
   };
