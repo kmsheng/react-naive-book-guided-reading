b821e0d27eb40bff3657395e97962fb56c6c37c2
kmsheng <kmsh3ng@gmail.com>
2017-06-12 19:04:42 +0800

    remove un-used variable createElement

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {Component, createElement} from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './index.css';
