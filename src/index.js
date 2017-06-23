0f5d3588ca84499fe43b9bacd2f4962bdbecea1e
kmsheng <kmsh3ng@gmail.com>
2017-06-24 00:47:56 +0800

    add redux + vanilla js example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+import {createStore} from 'redux';
+
+const initialState = 0;
+const ADD = 'react-naive-book-guided-reading/counter/ADD';
+
+function counterReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counterReducer);
+
+store.subscribe(() => {
+  const value = store.getState();
+  pad.innerHTML = value;
+});
+
 const pad = document.getElementById('pad');
 const btnAdd = document.getElementById('btn-add');
 
-let counterValue = 0;
-
 btnAdd.addEventListener('click', () => {
-  counterValue += 1;
-  pad.innerHTML = counterValue;
+  store.dispatch({type: ADD});
 });
