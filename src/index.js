085eb138ca30affa4a45afef374f7bd76e3c0f6c
kmsheng <kmsh3ng@gmail.com>
2017-06-25 11:14:42 +0800

    add Provider example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 import rootReducer from './redux/modules/rootReducer';
 import logger from './redux/middlewares/logger';
 import actionRecorder from './redux/middlewares/actionRecorder';
@@ -8,4 +9,10 @@ import App from './containers/App/App';
 
 const store = createStore(rootReducer, {counter: 10}, applyMiddleware(logger, actionRecorder));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const RootComponent = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(RootComponent, document.getElementById('root'));
