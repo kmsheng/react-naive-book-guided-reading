4546ccd4a9bb87cc25b99c224a9f6b5c129f9668
kmsheng <kmsh3ng@gmail.com>
2017-06-25 10:53:19 +0800

    re-style

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import actionRecorder from './redux/middlewares/actionRecorder';
 
 const store = createStore(rootReducer, {counter: 10}, applyMiddleware(logger, actionRecorder));
 
+const heading = document.getElementById('heading');
+const pad = document.getElementById('pad');
+const btnAdd = document.getElementById('btn-add');
+
 function render() {
   const state = store.getState();
   pad.innerHTML = state.counter;
 }
 
 store.subscribe(render);
-
-const heading = document.getElementById('heading');
-const pad = document.getElementById('pad');
-const btnAdd = document.getElementById('btn-add');
 
 btnAdd.addEventListener('click', () => {
   store.dispatch(add());
