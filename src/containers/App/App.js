d6a213a2d9e4c03ed3e5cf749404fffd922f47e8
kmsheng <kmsh3ng@gmail.com>
2017-06-25 11:30:34 +0800

    add react-redux example

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,34 @@
 import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {add} from './../../redux/modules/counter';
 import Counter from './../../components/Counter/Counter';
 
-export default class App extends Component {
+function mapStateToProps(state) {
+  return {counterValue: state.counter};
+}
 
-  add = () => {};
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({add}, dispatch);
+}
+
+export class App extends Component {
+
+  static propTypes = {
+    counterValue: PropTypes.number.isRequired,
+    add: PropTypes.func.isRequired
+  };
 
   render() {
+    const {add, counterValue} = this.props;
     return (
       <div>
         <h1>Hello World</h1>
-        <Counter add={this.add} value={0} />
+        <Counter add={add} value={counterValue} />
       </div>
     );
   }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
