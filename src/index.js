1d0946351153ef5b65859265814390754357e2d2
kmsheng <kmsh3ng@gmail.com>
2017-06-25 11:11:13 +0800

    update src/index.js (set up react.js stuff)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './redux/modules/rootReducer';
-import {add} from './redux/modules/counter';
 import logger from './redux/middlewares/logger';
 import actionRecorder from './redux/middlewares/actionRecorder';
+import App from './containers/App/App';
 
 const store = createStore(rootReducer, {counter: 10}, applyMiddleware(logger, actionRecorder));
 
-const heading = document.getElementById('heading');
-const pad = document.getElementById('pad');
-const btnAdd = document.getElementById('btn-add');
-
-function render() {
-  const state = store.getState();
-  pad.innerHTML = state.counter;
-  heading.innerHTML = state.main.projectName;
-}
-
-store.subscribe(render);
-
-btnAdd.addEventListener('click', () => {
-  store.dispatch(add());
-});
-
-render();
+ReactDOM.render(<App />, document.getElementById('root'));
